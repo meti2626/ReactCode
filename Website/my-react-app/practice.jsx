c2826aa2d12f1ff48fd0bd885d46992c5c0b2832
meti2626 <metseboka@gmail.com>
2025-04-19 23:09:27 +0300

    Update practice.jsx

--- Website/my-react-app/practice.jsx
+++ Website/my-react-app/practice.jsx
@@ -8,4 +8,18 @@ const MyComponent = () => {
   );
 };
 
+import React from 'react';
+import MyComponent from './MyComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Main App</h1>
+      <MyComponent />
+    </div>
+  );
+};
+
+export default App;
+
 export default MyComponent;
