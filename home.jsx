60634a3685be46cd0baad9a02683d8bb6987723e
meti2626 <metseboka@gmail.com>
2025-04-20 22:46:01 +0300

    Update home.jsx

--- home.jsx
+++ home.jsx
@@ -16,6 +16,19 @@ document
   function handleremoveFood ()
   {
     
+  return (
+  <div>
+    <h2>LIST OF FOOD</h2>
+    <ul>
+        {foods.map((food , index) => <li key={index}>{food}</li>)}
+    </ul>
+     
+
+    <input type="text" id="foodInput" placeholder="Enter food name"/>
+    <button onClick={handleAddFood}>Add Food</button>
+  </div>
+
+  )
   }
 
 
